bddac2eecbcde62fc58fae1312c31db12d8d1703
Noor Mohammed <noormohammedb@protonmail.com>
2023-05-15 17:37:53 +0400

    completed first challenge

--- exercises/ex00-testing/flipper/src/tests/flipper.rs
+++ exercises/ex00-testing/flipper/src/tests/flipper.rs
@@ -12,17 +12,32 @@ fn set_value_ok() {
 
 #[test]
 fn set_value_err_already_set() {
-	new_test_ext().execute_with(|| todo!("Verify if the function returns the expected error."));
+	new_test_ext().execute_with(|| {
+		// todo!("Verify if the function returns the expected error.")
+		assert_ok!(Flipper::set_value(Origin::signed(ALICE), false));
+		assert_noop!(
+			Flipper::set_value(Origin::signed(ALICE), true),
+			Error::<TestRuntime>::AlreadySet
+		);
+	});
 }
 
 #[test]
 fn flip_value_ok() {
-	new_test_ext()
-		.execute_with(|| todo!("Ensure the good behaviour of the flip_value() function."));
+	new_test_ext().execute_with(|| {
+		// todo!("Ensure the good behaviour of the flip_value() function.")
+		assert_ok!(Flipper::set_value(Origin::signed(ALICE), false));
+		assert_ok!(Flipper::flip_value(Origin::signed(ALICE)));
+	});
 }
 
 #[test]
 fn flip_value_ko() {
-	new_test_ext()
-		.execute_with(|| todo!("write a scenario that triggers an error in flip_value()"));
+	new_test_ext().execute_with(|| {
+		// todo!("write a scenario that triggers an error in flip_value()")
+		assert_noop!(
+			Flipper::flip_value(Origin::signed(ALICE)),
+			Error::<TestRuntime>::NoneValue
+		);
+	});
 }
