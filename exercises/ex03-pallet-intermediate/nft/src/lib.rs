f8ff8e1633cb10c5ab59b0f602d00d85694f1777
Noor Mohammed <noormohammedb@protonmail.com>
2023-05-17 00:38:40 +0400

    completed nft pallet
    fixed all the test with appropriate logic

--- exercises/ex03-pallet-intermediate/nft/src/lib.rs
+++ exercises/ex03-pallet-intermediate/nft/src/lib.rs
@@ -94,11 +94,77 @@ pub mod pallet {
 			metadata: BoundedVec<u8, T::MaxLength>,
 			supply: u128,
 		) -> DispatchResult {
+			// create UniqueAssetDetails with those data
+			// update UniqueAsset with the data
+			// update nonce
+			// mint in the account with inserting the data in Account
+			// emit created event
+
+			let origin = ensure_signed(origin)?;
+			ensure!(supply > 0, Error::<T>::NoSupply);
+			let asset_details =
+				UniqueAssetDetails::<T, T::MaxLength>::new(origin.clone(), metadata, supply);
+
+			let new_asset_id = Nonce::<T>::get();
+			UniqueAsset::<T>::insert(new_asset_id, asset_details);
+
+			let next_seet_id = new_asset_id + 1;
+			Nonce::<T>::set(next_seet_id);
+
+			Account::<T>::insert(new_asset_id, origin.clone(), supply);
+
+			Self::deposit_event(Event::<T>::Created {
+				asset_id: new_asset_id,
+				creator: origin,
+			});
+
 			Ok(())
 		}
 
 		#[pallet::weight(0)]
 		pub fn burn(origin: OriginFor<T>, asset_id: UniqueAssetId, amount: u128) -> DispatchResult {
+			// take asset count of the caller from Account, and burn with saturating_sub
+			// saturating_sub the total supply from the UniqueAsset's supply
+			// emit event
+
+			let origin = ensure_signed(origin)?;
+
+			ensure!(
+				UniqueAsset::<T>::contains_key(asset_id),
+				Error::<T>::UnknownAssetId
+			);
+
+			ensure!(
+				Account::<T>::get(asset_id, origin.clone()) > 0,
+				Error::<T>::NotOwned
+			);
+
+			let mut burned = 0;
+			let mut total_supply_at_last = 0;
+
+			Account::<T>::mutate(asset_id, origin.clone(), |asset_count| {
+				let initial_asset = asset_count.clone();
+				*asset_count = asset_count.saturating_sub(amount);
+				// burned = *asset_count - initial_asset;
+				burned = initial_asset.saturating_sub(*asset_count);
+			});
+
+			// pub(super) type UniqueAsset<T: Config> = StorageMap<_, Blake2_128Concat, UniqueAssetId, UniqueAssetDetails<T, T::MaxLength>>;
+
+			UniqueAsset::<T>::try_mutate(asset_id, |asset_arg| -> DispatchResult {
+				let asset_data = asset_arg.as_mut().ok_or(Error::<T>::UnknownAssetId)?;
+				asset_data.supply = asset_data.supply.saturating_sub(burned);
+
+				total_supply_at_last = asset_data.supply;
+				Ok(())
+			})?;
+
+			Self::deposit_event(Event::<T>::Burned {
+				asset_id,
+				owner: origin,
+				total_supply: total_supply_at_last,
+			});
+
 			Ok(())
 		}
 
@@ -109,6 +175,55 @@ pub mod pallet {
 			amount: u128,
 			to: T::AccountId,
 		) -> DispatchResult {
+			// saturating_sub caller's Account
+			// saturating_add to's Account with differents
+			// emit event
+
+			let origin = ensure_signed(origin)?;
+
+			ensure!(
+				UniqueAsset::<T>::contains_key(asset_id),
+				Error::<T>::UnknownAssetId
+			);
+
+			ensure!(
+				Account::<T>::get(asset_id, origin.clone()) > 0,
+				Error::<T>::NotOwned
+			);
+
+			// ensure!(
+			// 	Account::<T>::get(asset_id, origin.clone()) >= amount,
+			// 	Error::<T>::NotOwned
+			// );
+
+			let mut transfer_amount = 0;
+
+			Account::<T>::mutate(asset_id, origin.clone(), |asset_count| {
+				let initial_asset = asset_count.clone();
+				*asset_count = asset_count.saturating_sub(amount);
+				transfer_amount = initial_asset.saturating_sub(*asset_count);
+				dbg!(transfer_amount);
+			});
+
+			Account::<T>::mutate(asset_id, to.clone(), |asset_count| {
+				*asset_count = asset_count.saturating_add(transfer_amount);
+			});
+
+			Self::deposit_event(Event::<T>::Transferred {
+				asset_id,
+				from: origin,
+				to,
+				amount: transfer_amount,
+			});
+
+			/*
+			   Transferred {
+				   asset_id: UniqueAssetId,
+				   from: T::AccountId,
+				   to: T::AccountId,
+				   amount: u128,
+			   },
+			*/
 			Ok(())
 		}
 	}
