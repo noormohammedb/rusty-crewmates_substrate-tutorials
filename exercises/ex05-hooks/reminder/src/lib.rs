2d2eb6c1c9a1d8aef38c57fefc881a74f16d95a8
Noor Mohammed <noormohammedb@protonmail.com>
2023-05-20 23:20:53 +0400

    completed hooks
    transaction lifecycle
    emiting events on before block initialize and finalizing

--- exercises/ex05-hooks/reminder/src/lib.rs
+++ exercises/ex05-hooks/reminder/src/lib.rs
@@ -52,9 +52,10 @@ pub mod pallet {
 		fn on_initialize(n: T::BlockNumber) -> Weight {
 			let mut used_weight = 0;
 			// TODO: get the reminders for the block `n`
-			let reminders: Vec<Vec<u8>> = Vec::new();
+			// let reminders: Vec<Vec<u8>> = Vec::new();
+			let reminders: Vec<Vec<u8>> = Reminders::<T>::get(n);
 			// this is an example of how do we get system weights for read and writes.
-			// you only have to mesure read and writes for this exercice !
+			// you only have to measure read and writes for this exercice !
 			//
 			// try to do this hook in one read and two writes !
 			used_weight += T::DbWeight::get().reads(1);
@@ -63,16 +64,24 @@ pub mod pallet {
 			// find a way to count events for this block, and put the total in the
 			// corresponding storage
 
+			EventCounter::<T>::set(reminders.len() as u32);
+			used_weight += T::DbWeight::get().writes(1);
+
 			for reminder in reminders {
 				// TODO: now, emit a `Reminder` event for each events"
+				Self::deposit_event(Event::<T>::Reminder(reminder));
 			}
 
 			// TODO: clean the storage, a.k remove the events, after emitting them
+			Reminders::<T>::remove(n);
+			used_weight += T::DbWeight::get().writes(1);
+
 			used_weight
 		}
 
 		fn on_finalize(_: T::BlockNumber) {
 			// TODO: emit a `RemindersExecutes` event, with the right value
+			Self::deposit_event(Event::<T>::RemindersExecuteds(Self::event_counter()));
 		}
 	}
 
