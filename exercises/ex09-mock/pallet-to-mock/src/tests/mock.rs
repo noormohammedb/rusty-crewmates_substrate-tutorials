f171d578e3a3ebec33415297b783aaa7f01485df
Noor Mohammed <noormohammedb@protonmail.com>
2023-05-28 20:41:44 +0400

    basic boilerplate runtime for mock

--- exercises/ex09-mock/pallet-to-mock/src/tests/mock.rs
+++ exercises/ex09-mock/pallet-to-mock/src/tests/mock.rs
@@ -1,1 +1,69 @@
+use crate as pallet_to_mock;
+use frame_support::traits::{ConstU16, ConstU64};
+use sp_core::H256;
+use sp_runtime::{
+	testing::Header,
+	traits::{BlakeTwo256, IdentityLookup},
+};
 
+
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
+type Block = frame_system::mocking::MockBlock<Test>;
+
+frame_support::construct_runtime!(
+	pub enum Test where
+		UncheckedExtrinsic = UncheckedExtrinsic,
+		Block = Block,
+		NodeBlock = Block,
+		{
+			System: frame_system,
+			PalletToMock: pallet_to_mock,
+		}
+
+);
+
+impl frame_system::Config for Test {
+	type BaseCallFilter = frame_support::traits::Everything;
+	type BlockWeights = ();
+	type BlockLength = ();
+	type Origin = Origin;
+	type Call = Call;
+	type Index = u64;
+	type BlockNumber = u64;
+	type Hash = H256;
+	type Hashing = BlakeTwo256;
+	type AccountId = u64;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type Header = Header;
+	type Event = Event;
+	type BlockHashCount = ConstU64<250>;
+	type DbWeight = ();
+	type Version = ();
+	type PalletInfo = PalletInfo;
+	type AccountData = ();
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type SystemWeightInfo = ();
+	type SS58Prefix = ConstU16<42>;
+	type OnSetCode = ();
+	type MaxConsumers = frame_support::traits::ConstU32<16>;
+}
+
+use crate::PriceOracle;
+
+pub struct MyMock {}
+
+impl PriceOracle for MyMock{
+	type Error = ();
+	fn get_price() -> Result<u64, Self::Error>{
+		//
+		Ok(0)
+	}
+}
+
+impl pallet_to_mock::Config for Test {
+	type Event = Event;
+	type Currency = ();
+	type ValueToMint = ();
+	type SomePriceOracle = MyMock;
+}
