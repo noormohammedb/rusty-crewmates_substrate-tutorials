7358b3c14ccf3ae3fa8281d88cf48796bc4423d2
Noor Mohammed <noormohammedb@protonmail.com>
2023-05-29 01:04:21 +0400

    runtime mock makes compiler staisfied

--- exercises/ex09-mock/pallet-to-mock/src/tests/mock.rs
+++ exercises/ex09-mock/pallet-to-mock/src/tests/mock.rs
@@ -1,28 +1,36 @@
 use crate as pallet_to_mock;
-use frame_support::traits::{ConstU16, ConstU64};
+use frame_support::{parameter_types, traits::ConstU64};
 use sp_core::H256;
 use sp_runtime::{
 	testing::Header,
 	traits::{BlakeTwo256, IdentityLookup},
 };
 
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<TestRuntime>;
+type Block = frame_system::mocking::MockBlock<TestRuntime>;
 
-type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
-type Block = frame_system::mocking::MockBlock<Test>;
+type Balance = u64;
+type AccountId = u64;
+
+parameter_types! {
+  pub const ValueToMint: u64 = 200;
+	pub const BlockHashCount: u64 = 250;
+	pub const SS58Prefix: u8 = 42;
+}
 
 frame_support::construct_runtime!(
-	pub enum Test where
+	pub enum TestRuntime where
 		UncheckedExtrinsic = UncheckedExtrinsic,
 		Block = Block,
 		NodeBlock = Block,
 		{
 			System: frame_system,
+			Balances: pallet_balances,
 			PalletToMock: pallet_to_mock,
 		}
-
 );
 
-impl frame_system::Config for Test {
+impl frame_system::Config for TestRuntime {
 	type BaseCallFilter = frame_support::traits::Everything;
 	type BlockWeights = ();
 	type BlockLength = ();
@@ -32,38 +40,57 @@ impl frame_system::Config for Test {
 	type BlockNumber = u64;
 	type Hash = H256;
 	type Hashing = BlakeTwo256;
-	type AccountId = u64;
+	type AccountId = AccountId;
 	type Lookup = IdentityLookup<Self::AccountId>;
 	type Header = Header;
 	type Event = Event;
-	type BlockHashCount = ConstU64<250>;
+	type BlockHashCount = BlockHashCount;
 	type DbWeight = ();
 	type Version = ();
 	type PalletInfo = PalletInfo;
-	type AccountData = ();
+	type AccountData = pallet_balances::AccountData<Balance>;
 	type OnNewAccount = ();
 	type OnKilledAccount = ();
 	type SystemWeightInfo = ();
-	type SS58Prefix = ConstU16<42>;
+	type SS58Prefix = SS58Prefix;
 	type OnSetCode = ();
 	type MaxConsumers = frame_support::traits::ConstU32<16>;
+}
+
+impl pallet_balances::Config for TestRuntime {
+	type Balance = Balance;
+	type DustRemoval = ();
+	type Event = Event;
+	type ExistentialDeposit = ConstU64<1>;
+	type AccountStore = System;
+	type WeightInfo = ();
+	type MaxLocks = ();
+	type MaxReserves = ();
+	type ReserveIdentifier = [u8; 8];
+}
+impl pallet_to_mock::Config for TestRuntime {
+	type Event = Event;
+	type Currency = Balances;
+	type ValueToMint = ValueToMint;
+	type SomePriceOracle = MyMock;
 }
 
 use crate::PriceOracle;
 
 pub struct MyMock {}
 
-impl PriceOracle for MyMock{
+impl PriceOracle for MyMock {
 	type Error = ();
-	fn get_price() -> Result<u64, Self::Error>{
-		//
-		Ok(0)
+	fn get_price() -> Result<u64, Self::Error> {
+		Ok(1000)
 	}
 }
 
-impl pallet_to_mock::Config for Test {
-	type Event = Event;
-	type Currency = ();
-	type ValueToMint = ();
-	type SomePriceOracle = MyMock;
+pub fn new_test_ext() -> sp_io::TestExternalities {
+	frame_system::GenesisConfig::default()
+		.build_storage::<TestRuntime>()
+		.unwrap()
+		.into()
 }
+
+pub const ALICE: AccountId = 1;
